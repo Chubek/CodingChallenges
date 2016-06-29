0611b332aef33fda2c48065a3a17cfd9688916d6
Sami Dena <aurlito@riseup.net>
2016-06-29 06:31:39 +0430

    Add files via upload

---
+++ CountVowels/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] wordSplit = word.ToCharArray();
+            int vowelCount = 0;
+
+            for (int i = 0; i < wordSplit.Length; i++)
+            {
+                if (wordSplit[i] == 'e' || wordSplit[i] == 'a' || wordSplit[i] == 'u' || wordSplit[i] == 'i' || wordSplit[i] == 'o' ||
+                   wordSplit[i] == 'E' || wordSplit[i] == 'A' || wordSplit[i] == 'U' || wordSplit[i] == 'I' || wordSplit[i] == 'O')
+                {
+                    vowelCount += 1;
+                }
+                else
+                    continue;
+            }
+
+            Console.WriteLine(vowelCount);
+            Console.ReadKey();
+        }
+    }
+}
