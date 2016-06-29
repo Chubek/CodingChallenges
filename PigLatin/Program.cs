69315e007c64a5ff24ce4999ed1108c526dd8d72
Sami Dena <aurlito@riseup.net>
2016-06-29 06:37:36 +0430

    Add files via upload

---
+++ PigLatin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] charArray = word.ToCharArray();
+            string firstLetterPlusAy = charArray[0] + "ay";
+            string minusFirstLetter = word.Remove(0, 1);
+            Console.WriteLine(minusFirstLetter + firstLetterPlusAy);
+                       
+            
+            Console.ReadKey();
+        }
+    }
+}
