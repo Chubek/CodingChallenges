2ad9ddfafd1ca47ac3d516634c015884e4ce36ad
Sami Dena <aurlito@riseup.net>
2016-06-29 06:34:26 +0430

    Add files via upload

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Check
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            char[] wordSplit = word.ToCharArray();
+            Array.Reverse(wordSplit);
+            string reversedWord = new string(wordSplit);
+            string palindromeStatus;
+                        
+            if (word.Equals(reversedWord))
+            {
+                palindromeStatus = "is";
+            }
+            else
+            {
+                palindromeStatus = "is not";
+            }
+
+            
+            string wordUpperCase = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(word);
+
+            Console.WriteLine("{0} reversed is {1} which means {0} {2} a palindrome.", wordUpperCase, reversedWord, palindromeStatus);
+
+            Console.ReadKey();
+
+        }
+    }
+}
