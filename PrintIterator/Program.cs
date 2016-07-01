9c8d06e3ec97a0f75c85ecc164aebc449ce8b94f
Sami Dena <aurlito@riseup.net>
2016-07-01 13:53:31 +0430

    Add files via upload

---
+++ PrintIterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            char[] seperator = { ' ' };
+            int iterationFactor = 0;
+
+            string[] commandSplit = command.Split(seperator);
+
+            if (!(commandSplit[0] == "print"))
+                throw new Exception("Command must start with print");
+            else if (!(int.TryParse(commandSplit[1], out iterationFactor)))
+                throw new Exception("Iteration factor invalid");
+            else if (!(commandSplit.Length == 3))
+                throw new Exception("Syntax error");
+
+            for (int i = 0; i < iterationFactor; i++)
+            {
+                Console.WriteLine(commandSplit[2]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
