4c61b22cabba937526294a065af026f395097e2d
Sami Dena <aurlito@riseup.net>
2016-07-10 12:31:23 +0430

    Create Program.cs

---
+++ DecimalToBinary/Program.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    
+    class Program
+    {
+        public static List<int> returnList = new List<int>();
+        
+        static void Main(string[] args)
+        {
+            DecimalToBinary(156);
+        }
+
+        static List<int> DecimalToBinary(int deniry)
+        {
+            List<int> remainders = new List<int>();
+
+            int divided = Division(deniry);
+            
+
+            foreach (var number in returnList)
+            {
+                if (number % 2 == 0)
+                {
+                    remainders.Add(0);
+                }
+                else
+                {
+                    remainders.Add(1);
+                }
+            }
+            
+
+            remainders.Reverse();
+
+            return remainders;
+        }
+
+        static int Division(int number)
+        {
+            
+            int numberTwo = 0;
+
+            if (number != 1)
+            {
+                numberTwo = number / 2;
+                returnList.Add(numberTwo);
+                return Division(numberTwo);
+
+               
+               
+            }
+
+            else
+            {
+                return 1;
+            }
+           
+           
+            
+        }
+
+    }
+}
