e4c79ed8cf092a344974ccfb480373de8052dad1
Sami Dena <aurlito@riseup.net>
2018-11-08 19:37:25 +0330

    Update acid_base.cpp

--- AcidBases/acid_base.cpp
+++ AcidBases/acid_base.cpp
@@ -14,7 +14,29 @@ double pH(double hydronium)
 
 
 int main() {
-    std::cout << hydronium(6.3) << std::endl;
-    std::cout << pH(0.2) << std::endl;
+    std::cout << "1) Enter pH to calculate Hydronium (Enter 1 then press Enter)." << std::endl;
+    std::cout << "2) Enter Hydronium to calculate pH (Enter 2 then press Enter)." << std::endl;
+
+    double hydronium_ent, pH_ent;
+    int entery;
+
+    std::cin >> entery;
+
+    if (entery == 1)
+    {
+        std::cin >> pH_ent;
+        std::cout << "Hydronium is: " << hydronium(pH_ent) << std::endl;
+    }
+    else if (entery == 2)
+    {
+        std::cin >> hydronium_ent;
+        std::cout << "pH is: " << pH(hydronium_ent) << std::endl;
+    }
+    else
+    {
+        std::cerr << "Invaldi Entery;" << std::endl;
+    }
+
+
     return 0;
 }
