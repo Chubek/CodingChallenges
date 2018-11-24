ec4e298d2d7b74fe6b7c0ff9d9f8e41b980a1196
Chubak Bidpaa <aurlito@riseup.net>
2018-11-24 06:09:05 +0330

    Create led.cpp

---
+++ LED/led.cpp
@@ -0,0 +1,43 @@
+// RedBlueLED.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <string>
+#include <iostream>
+
+std::string LED(std::string red, std::string blue)
+{
+	int size = red.size();
+	std::string result;
+
+	for (int i = 0; i < size; ++i)
+	{
+		if (red[i] == '0' && blue[i] == '1')
+		{
+			result += 'R';
+		}
+		else if (red[i] == '1' && blue[i] == 0)
+		{
+			result += 'B';
+		}
+		else if (red[i] == '1' && blue[i] == '1')
+		{
+			result += 'R';
+		}
+		else
+		{
+			result += "";
+		}
+	}
+
+	return result;
+}
+
+
+int main()
+{
+	std::cout << LED("010101", "101010");
+    return 0;
+}
+
