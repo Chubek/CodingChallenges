6cde049062ab3b4c49bbad90ed5c5a526994fde0
Chubak Bidpaa <aurlito@riseup.net>
2018-11-24 07:14:43 +0330

    Update cached_fibonacci.py

--- CachedFibonacci/cached_fibonacci.py
+++ CachedFibonacci/cached_fibonacci.py
@@ -2,12 +2,13 @@ num_list = []
 def fibonacci_cached(n):
     if n == 0 or n == 1:
         return n
-    fib = fibonacci_cached(n-1)+fibonacci_cached(n-2)
-    if fib in num_list:
-        return fib
+
+    if n in num_list:
+        return n
     else:
+        fib = fibonacci_cached(n - 1) + fibonacci_cached(n - 2)
         num_list.append(fib)
         return fib
 
 
-print(fibonacci_cached(10))
+print(fibonacci_cached(55))
